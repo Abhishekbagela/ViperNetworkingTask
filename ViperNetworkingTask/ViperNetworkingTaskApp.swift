24f662c87ced50663d451cbd9d70cdaccfdeea2d
Abhishek-ab@ios-dev <abhibagela2015@gmail.com>
2026-04-22 19:40:09 +0530

    Initial Commit

---
+++ ViperNetworkingTask/ViperNetworkingTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViperNetworkingTaskApp.swift
+//  ViperNetworkingTask
+//
+//  Created by Abhishek Bagela on 22/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ViperNetworkingTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
